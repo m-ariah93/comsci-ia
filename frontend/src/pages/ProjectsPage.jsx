02d2584a4d6d56e6356e58df8fbd24b7f33ac17f
Mariah ♡ <msmit1119@eq.edu.au>
2025-12-17 16:56:33 +1000

    FIX: projects page nav vertical resizing

--- frontend/src/pages/ProjectsPage.jsx
+++ frontend/src/pages/ProjectsPage.jsx
@@ -5,12 +5,12 @@ export default function ProjectsPage() {
         <div>
             <div className="container-fluid d-flex flex-column vh-100">
                 <div className="row">
-                    <div className="d-grid gap-2 col-2 mx-auto">
-                        <NavLink to="/projects" end className={({ isActive }) => isActive ? "btn btn-primary text-start active" : "btn btn-primary text-start"}>Projects</NavLink>
+                    <div className="d-flex flex-column col-2 mx-auto">
+                        <NavLink to="/projects" end className={({ isActive }) => isActive ? "btn btn-primary text-start active" : "btn btn-primary text-start"}>All projects</NavLink>
                         <NavLink to="/projects/add" end className={({ isActive }) => isActive ? "btn btn-primary text-start active" : "btn btn-primary text-start"}>Add new project</NavLink>
                         <NavLink to="/projects/archive" end className={({ isActive }) => isActive ? "btn btn-primary active text-start" : "btn btn-primary text-start"}>View archive</NavLink>
                     </div>
-                    <main className="col-10">
+                    <main className="col-10 overflow-auto">
                         <Outlet />
                     </main>
                 </div>

989bfce14d48789d9ceef0d2a7cd9c0c56b40d69
Mariah ♡ <msmit1119@eq.edu.au>
2025-12-26 21:24:05 +1000

    FEAT: login page form (doesnt work YET)
    also the incorrect details alert shows by default oops

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,8 +1,29 @@
 export default function LoginPage() {
   return (
-    <div>
-        
+    <div className="d-flex justify-content-center align-items-center h-100">
+      <div className="card p-5">
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+          <strong>Uh oh!</strong> Your details are incorrect.
+          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+        <form>
+          <div className="mb-3">
+            <label htmlFor="inputUsername" className="form-label">Username</label>
+            <input type="text" className="form-control" id="inputUsername" aria-describedby="emailHelp" />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="inputPassword" className="form-label">Password</label>
+            <input type="password" className="form-control" id="inputPassword" />
+          </div>
+          <div className="mb-3 form-check">
+            <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+            <label className="form-check-label" htmlFor="exampleCheck1">Remember me</label>
+          </div>
+          <div className="d-grid">
+            <button className="btn btn-primary">Login</button>
+          </div>
+        </form>
+      </div>
     </div>
-    
   );
 }

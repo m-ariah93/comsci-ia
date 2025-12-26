39e88340d4f243be265f6539b2c6a0d5f1edd2af
Mariah ♡ <msmit1119@eq.edu.au>
2025-12-26 16:08:59 +1000

    FIX: project list scrolling

--- frontend/src/pages/ProjectsPage.jsx
+++ frontend/src/pages/ProjectsPage.jsx
@@ -4,13 +4,13 @@ export default function ProjectsPage() {
     return (
         <div>
             <div className="container-fluid d-flex flex-column vh-100">
-                <div className="row">
+                <div className="row h-100">
                     <div className="d-flex flex-column col-2 gap-2 mx-auto">
                         <NavLink to="/projects" end className={({ isActive }) => isActive ? "btn btn-primary text-start active" : "btn btn-primary text-start"}>All projects</NavLink>
                         <NavLink to="/projects/add" end className={({ isActive }) => isActive ? "btn btn-primary text-start active" : "btn btn-primary text-start"}>Add new project</NavLink>
                         <NavLink to="/projects/archive" end className={({ isActive }) => isActive ? "btn btn-primary active text-start" : "btn btn-primary text-start"}>View archive</NavLink>
                     </div>
-                    <main className="col-10 overflow-auto pe-5">
+                    <main className="col-10 overflow-auto pe-5 vh-100">
                         <Outlet />
                     </main>
                 </div>

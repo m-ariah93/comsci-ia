296b33d621079c9367b376abc9bc971396b60f76
Mariah ♡ <msmit1119@eq.edu.au>
2025-12-27 15:22:49 +1000

    stretch checkbox link over entire <li>

--- frontend/src/pages/ChecklistPage.jsx
+++ frontend/src/pages/ChecklistPage.jsx
@@ -39,13 +39,19 @@ export default function ChecklistPage() {
         <div className="container-fluid d-flex flex-column vh-100 pe-4" style={{ minHeight: 0 }}>
             <h4>Order checklist</h4>
             <ul className="list-group overflow-auto flex-grow-1" style={{ minHeight: 0 }}>
-                {checklist.map((item) => (
-                    <li className='list-group-item'>
-                        <input className="form-check-input me-2" type="checkbox" value="" id="firstCheckbox" />
-                        <label className="form-check-label" htmlFor="firstCheckbox">{item}</label>
+                {checklist.map((item, i) => (
+                    <li key={`check-${i}`} className='list-group-item position-relative'>
+                        <input className="form-check-input me-2" type="checkbox" value="" id={`check-${i}`} />
+                        <label className="form-check-label" htmlFor={`check-${i}`}>{item}</label>
+                        <label
+                            htmlFor={`check-${i}`}
+                            className="stretched-link"
+                            style={{ cursor: "pointer" }}
+                        />
                     </li>
                 ))}
             </ul>
         </div>
+
     );
 }

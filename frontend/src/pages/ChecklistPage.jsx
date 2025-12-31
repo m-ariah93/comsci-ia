7585b2eb9ba5ce4df79be71ba5b995d5ea0189e1
Mariah ♡ <msmit1119@eq.edu.au>
2025-12-31 16:33:02 +1000

    FIX: alert on checklist page if no projects

--- frontend/src/pages/ChecklistPage.jsx
+++ frontend/src/pages/ChecklistPage.jsx
@@ -40,37 +40,47 @@ export default function ChecklistPage() {
     ];
 
     const { activeProjects } = useProjects();
-    const [currentProject, setCurrentProject] = useState(activeProjects[0].id);
+    const [currentProject, setCurrentProject] = useState(
+        activeProjects.length > 0 ? activeProjects[0].id : null
+    );
 
     const location = useLocation();
 
     return (
-        <div className="container-fluid d-flex flex-column vh-100 pe-4" style={{ minHeight: 0 }}>
-            <div className="d-flex flex-nowrap my-2">
-                <ul className="nav nav-tabs overflow-x-auto overflow-y-hidden flex-nowrap text-nowrap" id="navTabsHorizontal">
-                    {activeProjects.map((project) => (
-                        <li className="nav-item" key={project.id}>
-                            <a className={`nav-link ${currentProject === project.id ? 'active fw-semibold' : ''}`} href="#" style={{ color: project.colour }} onClick={() => setCurrentProject(project.id)}>{project.title}</a>
-                        </li>
-                    ))}
-                </ul>
-                <Link to="/projects/add" state={{ from: location }} className="btn btn-outline-primary ms-auto"><img src={plusIcon} /></Link>
-            </div>
-            <h4>Order checklist</h4>
-            <ul className="list-group overflow-auto flex-grow-1 pb-4" style={{ minHeight: 0 }}>
-                {checklist.map((item, i) => (
-                    <li key={`check-${i}`} className='list-group-item position-relative'>
-                        <input className="form-check-input me-2" type="checkbox" value="" id={`check-${i}`} />
-                        <label className="form-check-label" htmlFor={`check-${i}`}>{item}</label>
-                        <label
-                            htmlFor={`check-${i}`}
-                            className="stretched-link"
-                            style={{ cursor: "pointer" }}
-                        />
-                    </li>
-                ))}
-            </ul>
-        </div>
-
+        <>
+            {/* if no projects, show message */}
+            {activeProjects.length === 0 ? (
+                <div className="alert alert-primary text-center mx-auto" role="alert">
+                    No projects found :( Create one <Link to="/projects/add" className="alert-link text-reset">here</Link>.
+                </div>
+            ) : (
+                <div className="container-fluid d-flex flex-column vh-100 pe-4" style={{ minHeight: 0 }}>
+                    <div className="d-flex flex-nowrap my-2">
+                        <ul className="nav nav-tabs overflow-x-auto overflow-y-hidden flex-nowrap text-nowrap" id="navTabsHorizontal">
+                            {activeProjects.map((project) => (
+                                <li className="nav-item" key={project.id}>
+                                    <a className={`nav-link ${currentProject === project.id ? 'active fw-semibold' : ''}`} href="#" style={{ color: project.colour }} onClick={() => setCurrentProject(project.id)}>{project.title}</a>
+                                </li>
+                            ))}
+                        </ul>
+                        <Link to="/projects/add" state={{ from: location }} className="btn btn-outline-primary ms-auto"><img src={plusIcon} /></Link>
+                    </div>
+                    <h4>Order checklist</h4>
+                    <ul className="list-group overflow-auto flex-grow-1 pb-4" style={{ minHeight: 0 }}>
+                        {checklist.map((item, i) => (
+                            <li key={`check-${i}`} className='list-group-item position-relative'>
+                                <input className="form-check-input me-2" type="checkbox" value="" id={`check-${i}`} />
+                                <label className="form-check-label" htmlFor={`check-${i}`}>{item}</label>
+                                <label
+                                    htmlFor={`check-${i}`}
+                                    className="stretched-link"
+                                    style={{ cursor: "pointer" }}
+                                />
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )};
+        </>
     );
 }

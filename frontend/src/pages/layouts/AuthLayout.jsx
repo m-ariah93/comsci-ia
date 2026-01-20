b9995c61a7c10aaa0819655af717b60ab446744d
Mariah ♡ <msmit1119@eq.edu.au>
2026-01-20 15:03:57 +1000

    colour change and login gradient

--- frontend/src/pages/layouts/AuthLayout.jsx
+++ frontend/src/pages/layouts/AuthLayout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 export default function AuthLayout() {
   return (
-    <div className="w-100 vh-100">
+    <div className="w-100 vh-100" style={{ backgroundImage: "linear-gradient(150deg, #4A7E8D, #A3BDC3)" }}>
       <Outlet />
     </div>
   );

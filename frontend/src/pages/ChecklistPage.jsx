9d87da83e847efd63b5d6c8de26e1efff6ba3d78
Mariah ♡ <msmit1119@eq.edu.au>
2026-04-06 10:55:44 +1000

    add checklist note button

--- frontend/src/pages/ChecklistPage.jsx
+++ frontend/src/pages/ChecklistPage.jsx
@@ -75,6 +75,8 @@ export default function ChecklistPage() {
                                 <li key={`check-${i}`} className='list-group-item position-relative'>
                                     <input className="form-check-input me-2" type="checkbox" value="" id={`check-${i}`} onChange={(e) => onChecklistChange(item.id, e.target.checked)} checked={Boolean(item.done)} />
                                     <label className={`form-check-label ${item.done && "text-decoration-line-through"}`} htmlFor={`check-${i}`}>{item.title}</label>
+                                    <button className="btn btn-outline-primary rounded-circle align-items-center justify-content-center ms-auto me-3"><i className="bi bi-sticky fs-4"></i></button>
+
                                     <label
                                         htmlFor={`check-${i}`}
                                         className="stretched-link"

f20d1a1c5b528d390f3f0b3d4dc6b374bf113eae
Mariah ♡ <msmit1119@eq.edu.au>
2026-04-08 15:45:03 +1000

    even more spacing fixes

--- frontend/src/pages/ProjectsPage.jsx
+++ frontend/src/pages/ProjectsPage.jsx
@@ -4,13 +4,13 @@ export default function ProjectsPage() {
     return (
         <>
             <div className="container-fluid vh-100 px-0">
-                <div className="d-flex flex-column flex-md-row h-100 pt-3">
-                    <div className="d-flex flex-column nav-pills gap-2 mb-3 w-100" role="tablist" aria-orientation="vertical" style={{ maxWidth: "180px", minWidth: "180px" }}>
+                <div className="d-flex flex-column flex-md-row h-100">
+                    <div className="d-flex flex-column nav-pills gap-2 mb-3 w-100 pt-3" role="tablist" aria-orientation="vertical" style={{ maxWidth: "180px", minWidth: "180px" }}>
                         <NavLink to="/projects" end className={({ isActive }) => isActive ? "nav-link active py-2 px-3" : "nav-link text-primary py-2 px-3"}>Active projects</NavLink>
                         <NavLink to="/projects/add" end className={({ isActive }) => isActive ? "nav-link active py-2 px-3" : "nav-link text-primary py-2 px-3"}>Add new project</NavLink>
                         <NavLink to="/projects/archive" end className={({ isActive }) => isActive ? "nav-link active py-2 px-3" : "nav-link text-primary py-2 px-3"}>View archive</NavLink>
                     </div>
-                    <main className="flex-grow-1 overflow-y-auto overflow-x-hidden h-100 px-0 pb-3 ps-4">
+                    <main className="flex-grow-1 overflow-y-auto overflow-x-hidden h-100 px-0 pb-3 ps-4 pt-3">
                         <div className="pe-3 pe-md-4 sticky-top">
                             <hr className="w-100 mt-0 d-block d-md-none" />
                         </div>

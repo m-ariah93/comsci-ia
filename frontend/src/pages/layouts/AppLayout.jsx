57a9abffac5e7368cf4c27d51b3af0a3416cc317
m-ariah93 <msmit1119@eq.edu.au>
2026-04-10 23:24:05 +1000

    make nav icons bigger

--- frontend/src/pages/layouts/AppLayout.jsx
+++ frontend/src/pages/layouts/AppLayout.jsx
@@ -10,24 +10,24 @@ export default function AppLayout() {
                 <div className="nav flex-column nav-pills mx-2 mx-sm-3 pt-3 gap-3 gap-sm-2" role="tablist" aria-orientation="vertical">
                     <NavLink to="/calendar" end className={({ isActive }) => isActive ? "nav-link text-center active" : "nav-link text-center"}>
                         <span className="d-none d-sm-inline">Calendar</span>
-                        <i className="bi bi-calendar-event-fill d-inline d-sm-none"></i>
+                        <i className="bi bi-calendar-event-fill d-inline d-sm-none fs-5"></i>
                     </NavLink>
                     <NavLink to="/checklist" className={({ isActive }) => isActive ? "nav-link text-center active" : "nav-link text-center"}>
                         <span className="d-none d-sm-inline">Checklist</span>
-                        <i className="bi bi-clipboard2-check-fill d-inline d-sm-none"></i>
+                        <i className="bi bi-clipboard2-check-fill d-inline d-sm-none fs-5"></i>
                     </NavLink>
                     <NavLink to="/projects" className={({ isActive }) => isActive ? "nav-link text-center active" : "nav-link text-center"}>
                         <span className="d-none d-sm-inline">Projects</span>
-                        <i className="bi bi-grid-fill d-inline d-sm-none"></i>
+                        <i className="bi bi-grid-fill d-inline d-sm-none fs-5"></i>
                     </NavLink>
                     <NavLink to="/settings" className={({ isActive }) => isActive ? "nav-link text-center active" : "nav-link text-center"}>
                         <span className="d-none d-sm-inline">Settings</span>
-                        <i className="bi bi-gear-fill d-inline d-sm-none"></i>
+                        <i className="bi bi-gear-fill d-inline d-sm-none fs-5"></i>
                     </NavLink>
                     <hr />
                     <button className="nav-link text-center" onClick={logout}>
                         <span className="d-none d-sm-inline">Logout</span>
-                        <i className="bi bi-box-arrow-right d-inline d-sm-none"></i>
+                        <i className="bi bi-box-arrow-right d-inline d-sm-none fs-5"></i>
                     </button>
                 </div>
             </div>

e1c05ca913b89593f3b1965080b0385d94b97c30
m-ariah93 <msmit1119@eq.edu.au>
2026-04-10 16:21:51 +1000

    prevent date popover click focus

--- frontend/src/pages/CalendarPage.jsx
+++ frontend/src/pages/CalendarPage.jsx
@@ -248,7 +248,7 @@ export default function CalendarPage() {
                                 <div className="overflow-auto border d-grid gap-0 row-gap-1 mt-2" ref={keyBookingsRef}>
                                     {templateBookings.map((event) => (
                                         event.used ? (
-                                            <div key={event.bookingId} className="d-inline-block" tabIndex="0" data-bs-toggle="popover" data-bs-trigger="hover focus" data-bs-placement="left" data-bs-content={formatDate(event.start)}>
+                                            <div key={event.bookingId} className="d-inline-block" tabIndex="0" data-bs-toggle="popover" data-bs-trigger="hover" data-bs-placement="left" data-bs-content={formatDate(event.start)}>
                                                 <DraggableEventComponent event={event} />
                                             </div>
                                         ) : (

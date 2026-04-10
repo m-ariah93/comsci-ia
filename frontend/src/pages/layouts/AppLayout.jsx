f85871efed2875b73e0b821419cc8add582f6ed7
m-ariah93 <msmit1119@eq.edu.au>
2026-04-10 21:37:12 +1000

    test responsive nav icon

--- frontend/src/pages/layouts/AppLayout.jsx
+++ frontend/src/pages/layouts/AppLayout.jsx
@@ -7,14 +7,17 @@ export default function AppLayout() {
     return (
         <div className="d-flex align-items-start vh-100">
             <div className="h-100 me-3" style={{ backgroundColor: "#dbe5e8" }}>
-            <div className="nav flex-column nav-pills mx-3 pt-3 gap-2" role="tablist" aria-orientation="vertical">
-                <NavLink to="/calendar" end className={({ isActive }) => isActive ? "nav-link text-center active" : "nav-link text-center"}>Calendar</NavLink>
-                <NavLink to="/checklist" className={({ isActive }) => isActive ? "nav-link text-center active" : "nav-link text-center"}>Checklist</NavLink>
-                <NavLink to="/projects" className={({ isActive }) => isActive ? "nav-link text-center active" : "nav-link text-center"}>Projects</NavLink>
-                <NavLink to="/settings" className={({ isActive }) => isActive ? "nav-link text-center active" : "nav-link text-center"}>Settings</NavLink>
-                <hr />
-                <button className="nav-link text-center" onClick={logout}>Logout</button>
-            </div>
+                <div className="nav flex-column nav-pills mx-3 pt-3 gap-2" role="tablist" aria-orientation="vertical">
+                    <NavLink to="/calendar" end className={({ isActive }) => isActive ? "nav-link text-center active" : "nav-link text-center"}>Calendar</NavLink>
+                    <NavLink to="/checklist" className={({ isActive }) => isActive ? "nav-link text-center active" : "nav-link text-center"}>Checklist</NavLink>
+                    <NavLink to="/projects" className={({ isActive }) => isActive ? "nav-link text-center active" : "nav-link text-center"}>Projects</NavLink>
+                    <NavLink to="/settings" className={({ isActive }) => isActive ? "nav-link text-center active" : "nav-link text-center"}>
+                        <span className="d-none d-md-inline">Settings</span>
+                        <i className="bi bi-gear-fill d-inline d-md-none"></i>
+                    </NavLink>
+                    <hr />
+                    <button className="nav-link text-center" onClick={logout}>Logout</button>
+                </div>
             </div>
 
             <div className="tab-content w-100 flex-grow-1 d-flex flex-column vh-100" id="v-pills-tabContent" style={{ minWidth: 0 }}>

e62f89df9ba10bff32f0e3a68a94a66554c304a8
m-ariah93 <msmit1119@eq.edu.au>
2026-04-12 18:06:03 +1000

    email save success toast notif

--- frontend/src/pages/SettingsPage.jsx
+++ frontend/src/pages/SettingsPage.jsx
@@ -172,7 +172,7 @@ export default function SettingsPage() {
 				<div id="emailSavedToast" className="toast align-items-center text-bg-success border-0" role="alert" aria-live="assertive" aria-atomic="true" data-bs-autohide="true">
 					<div className="d-flex">
 						<div className="toast-body">
-							Default email settings successfully updated.
+							Default email settings successfully saved.
 						</div>
 						<button type="button" className="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
 					</div>

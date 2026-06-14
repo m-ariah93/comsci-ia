ae7184e09c77fe6d4bb0e0fcd4b9ef310c3b45da
m-ariah93 <msmit1119@eq.edu.au>
2026-06-14 13:30:33 +1000

    fix: same invalid feedback issue on edit form

--- frontend/src/pages/projects/EditProject.jsx
+++ frontend/src/pages/projects/EditProject.jsx
@@ -55,7 +55,6 @@ export default function EditProject() {
         }
 
         saveProject(project.id, title, address, startMonth, colour);
-        // console.log("Saved project");
         navigate("/projects", { state: { updated: true } }); // go back to projects list
     }
 
@@ -77,22 +76,30 @@ export default function EditProject() {
                     </div>
                 ) : (
                     <>
-                        <label htmlFor="titleInput" className="form-label">Title (max 30 characters)</label>
-                        <input type="text" maxLength={TITLE_MAX_LENGTH} className="form-control" id="titleInput" value={title} onChange={(e) => setTitle(e.target.value)} required />
-                        <div className="invalid-feedback">
-                            Please enter a project title.
+                        <div>
+                            <label htmlFor="titleInput" className="form-label">Title (max 30 characters)</label>
+                            <input type="text" maxLength={TITLE_MAX_LENGTH} className="form-control" id="titleInput" value={title} onChange={(e) => setTitle(e.target.value)} required />
+                            <div className="invalid-feedback">
+                                Please enter a project title.
+                            </div>
                         </div>
 
-                        <label htmlFor="addressInput" className="form-label mt-3">Address</label>
-                        <input type="text" className="form-control" id="addressInput" value={address} onChange={(e) => setAddress(e.target.value)} required />
-                        <div className="invalid-feedback">
-                            Please enter an address.
+                        <div>
+                            <label htmlFor="addressInput" className="form-label mt-3">Address</label>
+                            <input type="text" className="form-control" id="addressInput" value={address} onChange={(e) => setAddress(e.target.value)} required />
+                            <div className="invalid-feedback">
+                                Please enter an address.
+                            </div>
                         </div>
-                        <label htmlFor="startMonthInput" className="form-label mt-3">Start month</label>
-                        <input type="month" className="form-control" style={{ width: 200 }} id="startMonthInput" value={startMonth} onChange={(e) => setStartMonth(e.target.value)} required></input>
-                        <div className="invalid-feedback">
-                            Please select the project's start month.
+
+                        <div>
+                            <label htmlFor="startMonthInput" className="form-label mt-3">Start month</label>
+                            <input type="month" className="form-control" style={{ width: 200 }} id="startMonthInput" value={startMonth} onChange={(e) => setStartMonth(e.target.value)} required></input>
+                            <div className="invalid-feedback">
+                                Please select the project's start month.
+                            </div>
                         </div>
+
                         <label htmlFor="colourInput" className="form-label mt-3">Colour (for calendar events)</label>
                         <input type="color" className="form-control form-control-color mb-3" title="Choose your colour" id="colourInput" value={colour} onChange={(e) => setColour(e.target.value)}></input>
                         <button type="submit" className="btn btn-primary me-2">Save changes</button>
@@ -107,7 +114,7 @@ export default function EditProject() {
                     </>
                 )}
             </form>
-            {project && <DeleteModal projectId={project.id} onDelete={handleDeleteProject} onArchive={handleArchiveProject}/>}
+            {project && <DeleteModal projectId={project.id} onDelete={handleDeleteProject} onArchive={handleArchiveProject} />}
         </>
     );
 }
